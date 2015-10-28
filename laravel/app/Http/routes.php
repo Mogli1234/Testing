4b681794104ca809e7458461b3f3b1f26a273c61
Jaime A. Esquivel <jaimeesquivelgomez@gmail.com>
2015-10-27 22:49:04 -0600

    dasdas

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -18,6 +18,7 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
+
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::get('/', function () {
